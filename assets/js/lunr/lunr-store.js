3687c6e9d1f00aaa508b6f198d655ac7ed44b940
shulamula <255425851+shulamula@users.noreply.github.com>
2026-08-22 00:25:43 +0600

    dg

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,111 @@
+var store = [{
+        "title": "The First Call for Papers is Out!",
+        "excerpt":"Researchers may have just arrived home from a fantastic ACL conference in Florence, but it is already time to start thinking about Seattle 2020. The First Call for Papers is now out! And your paper is due on Monday, December 9! This first in a series of ACL 2020 PC...","categories": ["blog"],
+        "tags": ["papers","deadline","publication","tracks","visa","reviewing","theme"],
+        "url": "https://acl2020.org/blog/the-first-call-for-papers-is-out/",
+        "teaser":null},{
+        "title": "PC Blog Post on Theme",
+        "excerpt":"Our field is growing fast! This is an exciting time to do NLP research. But at this pace of growth it’s only healthy as a community to take stock of where we’ve been and chart out where we should be going. So we made it a theme. For the first...","categories": ["blog"],
+        "tags": ["papers","tracks","theme"],
+        "url": "https://acl2020.org/blog/theme/",
+        "teaser":null},{
+        "title": "Archive Papers and Double Sumissions",
+        "excerpt":"FAQ for archive papers and double submissions: (new) Q6: Is the November 9 deadline a submission deadline or a publication deadline? A6: For arXiv submissions, November 9 11:59pm UTC-12h (anywhere on earth) is the submission deadline. The actual publication on the archive may take a day or two after that....","categories": ["blog"],
+        "tags": ["papers","FAQ"],
+        "url": "https://acl2020.org/blog/arxiv-papers-double-submissions/",
+        "teaser":null},{
+        "title": "PC Blog Post on Author/Reviewer Profile",
+        "excerpt":"We have introduced a new sign-up process for authors and reviewers at ACL20, this blog post provides detailed instructions on how to sign up (Part 1) and answers some commonly asked questions (Part 2). 1. ACL2020 Instructions for Authors and Reviewers: Profile Setup As our field has grown considerably over...","categories": ["blog"],
+        "tags": ["papers","FAQ"],
+        "url": "https://acl2020.org/blog/author-reviewer-profile/",
+        "teaser":null},{
+        "title": "Improving ACL Conflict-of-interest Detection and Reviewer Matching",
+        "excerpt":"We all know that ACL conferences are getting bigger, and this is great: reflecting a burgeoning and vigorous community. At the same time, it also means that the amount of work that goes into managing various aspects of the conference is growing greatly as well. In particular, ensuring an effective...","categories": ["blog"],
+        "tags": ["paper","reviewing"],
+        "url": "https://acl2020.org/blog/conflict-of-interest/",
+        "teaser":null},{
+        "title": "PC Blog Post Six Days Before Deadline!",
+        "excerpt":"We are only six days away from the submission deadline!! ACL 2020 is experimenting with several new implementations to keep up with the growing number of submissions. First of all, we would like to thank you for your patience and cooperation! Here are a couple reminders/clarifications regarding the Global Profile...","categories": ["blog"],
+        "tags": ["papers","FAQ"],
+        "url": "https://acl2020.org/blog/six-days-before-deadline/",
+        "teaser":null},{
+        "title": "Blog Post by Remote Presentation Chairs",
+        "excerpt":"As with previous conferences, ACL 2020 will allow for remote paper presentations (both oral and poster) in circumstances where none of the authors can attend the conference due to: failure to be awarded a visa (either denied or delayed despite good faith efforts to apply on time) or government travel...","categories": ["blog"],
+        "tags": ["presentations"],
+        "url": "https://acl2020.org/blog/remote-presentations/",
+        "teaser":null},{
+        "title": "Global Profile FAQs",
+        "excerpt":"Hi all, congratulations on making it through the ACL deadline! As you know, we’re using a collection of new methods (Global Profile and an Author/Reviewer Form) to improve conflict of interest detection as well as better match reviewers with papers. A few common questions appeared in the last week on...","categories": ["blog"],
+        "tags": ["global-profile","faqs"],
+        "url": "https://acl2020.org/blog/global-profile-faqs/",
+        "teaser":null},{
+        "title": "PC Blog Post - Start of Review Process",
+        "excerpt":"We have finally kicked off the reviewing process (January 17 to February 07)!! As you know, we are experimenting with several conference process changes this year: (1) a COI detection system developed by Amanda Stent and Arya McCarthy, (2) a reviewer-paper matching system developed by Graham Neubig, and (3) removed...","categories": ["blog"],
+        "tags": ["papers","FAQ"],
+        "url": "https://acl2020.org/blog/start-of-review-process/",
+        "teaser":null},{
+        "title": "The ACL 2020 Diversity and Inclusion Committee",
+        "excerpt":"ACL 2020 is committed to providing an inclusive conference to participants with diverse needs, academic interests, and socio-demographic backgrounds. There are numerous barriers that limit different participants’ ability to attend our conferences. Our whole community benefits when we strive to remove those barriers and everyone feels welcomed at the conference...","categories": ["blog"],
+        "tags": ["diversity-inclusion"],
+        "url": "https://acl2020.org/blog/diversity-and-inclusion/",
+        "teaser":null},{
+        "title": "Accessibility for Camera-Ready Papers",
+        "excerpt":"As a leading venue for published research on NLP/CL, it is important that we as a community do our best effort to make ACL 2020 papers accessible to our colleagues with disabilities. Here are some suggestions to consider when preparing your camera-ready paper (and supplementary material): Use high resolution images...","categories": ["blog"],
+        "tags": ["diversity-inclusion,","accessibility"],
+        "url": "https://acl2020.org/blog/accessibility-for-camera-ready/",
+        "teaser":null},{
+        "title": "Call for Volunteers",
+        "excerpt":"Submission Deadline: Friday, June 9 June 12, 2020 NOTE: Those who already volunteered following our earlier call for website development volunteers, can we trouble you to please fill out this Google form or this Microsoft Office form (incase the Google form is not accessible) so we can ensure you get...","categories": ["blog"],
+        "tags": ["volunteers"],
+        "url": "https://acl2020.org/blog/call-for-volunteers/",
+        "teaser":null},{
+        "title": "Introduction to the Main Conference Infrastructure",
+        "excerpt":"Program Chairs (Joyce Chai, Natalie Schluter, and Joel Tetreault) and General Chair (Dan Jurafsky), for the entire Organizing Committee We’re excited to give you some more details on the virtual infrastructure for ACL 2020! In this post, we’ll summarize the main conference, which will take place July 6-8; more information...","categories": ["blog"],
+        "tags": ["virtual","program"],
+        "url": "https://acl2020.org/blog/intro-to-conference-infrastructure/",
+        "teaser":null},{
+        "title": "Accessible Presentations at ACL 2020",
+        "excerpt":"Virtual presentations will change some of the experience of ACL, but not the imperative to make your work more accessible to a wide audience. The prerecorded nature of many of these talks will even create new opportunities for inclusion. Talks can be attended in other time zones, be rewinded to...","categories": ["blog"],
+        "tags": ["diversity-inclusion,","accessibility"],
+        "url": "https://acl2020.org/blog/Accessible-Presentations-at-ACL-2020/",
+        "teaser":null},{
+        "title": "ACL2020: General Conference Statistics",
+        "excerpt":"There is just over one month to go before ACL 2020 (July 5-10)! We recently announced the list of accepted papers and wanted to start sharing some statistics around submissions, reviews and other interesting topics over the next month. In this blog post we focus on acceptance rates for ACL2020,...","categories": ["blog"],
+        "tags": ["papers","tracks","theme"],
+        "url": "https://acl2020.org/blog/general-conference-statistics/",
+        "teaser":null},{
+        "title": "Call for Live Microblogging Volunteers",
+        "excerpt":"Live tweeting ACL 2020 // Author Option to Opt-Out Call for Volunteers We are seeking volunteers to live tweet/live microblog ACL 2020 presentations with the goal of raising the visibility of ACL 2020 talks, across platforms and across languages. Specifically, we envision live microblogging of pre-recorded presentations (between when they...","categories": ["blog"],
+        "tags": ["volunteers,","twitter,","weibo"],
+        "url": "https://acl2020.org/blog/call-for-live-microblogging-volunteers/",
+        "teaser":null},{
+        "title": "Introduction to the organization and infrastructure of ACL 2020 tutorials",
+        "excerpt":"Tutorial Chairs (Agata Savary and Yue Zhang) and General Chair (Dan Jurafsky), for the entire Organizing Committee NEW: Checkout the new blogpost by tutorial chairs describing detailed modalities of ACL2020 tutorials. It is our pleasure to give you some more details on the organization and the virtual infrastructure of ACL...","categories": ["blog"],
+        "tags": ["virtual","infrastructure","tutorials"],
+        "url": "https://acl2020.org/blog/intro-to-tutorial-infrastructure/",
+        "teaser":null},{
+        "title": "Tips for talking to the media",
+        "excerpt":"Below are some tips for talking about your research with the press, from the ACL 2020 Publicity Chairs (Emily M. Bender, Esther Seyffarth, and Zhiyuan Liu) Check with your employer/institution first to see if there are any constraints on your speaking with the press about your research. They may also...","categories": ["blog"],
+        "tags": ["media"],
+        "url": "https://acl2020.org/blog/tips-for-talking-to-the-media/",
+        "teaser":null},{
+        "title": "Virtual Format of Plenary Sessions",
+        "excerpt":"The first virtual ACL is quickly approaching! Several weeks ago we described the format of this new conference in a blog post. In this post, we wanted to give you more details about one aspect of the conference, the Plenary Sessions. All three days of the main conference feature a...","categories": ["blog"],
+        "tags": ["virtual","program"],
+        "url": "https://acl2020.org/blog/virtual-format-of-plenary-sessions/",
+        "teaser":null},{
+        "title": "Detailed modalities of the ACL 2020 tutorials",
+        "excerpt":"Tutorial Chairs (Agata Savary and Yue Zhang) and General Chair (Dan Jurafsky), for the entire Organizing Committee ACL 2020 features 8 tutorials on July 5. See the schedule and our previous post for the introduction to the tutorial organisation and infrastructure. Here we give some further details of the tutorial...","categories": ["blog"],
+        "tags": ["virtual","infrastructure","tutorials","modalities"],
+        "url": "https://acl2020.org/blog/detailed-modalities-of-tutorials/",
+        "teaser":null},{
+        "title": "Increasing financial accessibility of ACL 2020",
+        "excerpt":"The Financial Access subcommittee of the ACL 2020 Diversity and Inclusion initiative has been working to increase conference access by reducing the financial burden for conference attendees – particularly researchers from underrepresented geographical regions and demographics. With the ACL 2020 virtual conference, the contributions of the Financial Access subcommittee fell...","categories": ["blog"],
+        "tags": ["diversity-inclusion"],
+        "url": "https://acl2020.org/blog/increasing-financial-accessibility/",
+        "teaser":null},{
+        "title": "Best Paper Awards at ACL 2020",
+        "excerpt":"Congratulations to the recipients of the best paper awards at ACL 2020! Best Paper Beyond Accuracy: Behavioral Testing of NLP Models with CheckList Marco Tulio Ribeiro, Tongshuang Wu, Carlos Guestrin and Sameer Singh Honorable Mention Papers – Main Conference Don’t Stop Pretraining: Adapt Language Models to Domains and Tasks Suchin...","categories": ["blog"],
+        "tags": ["best-paper-awards"],
+        "url": "https://acl2020.org/blog/ACL-2020-best-papers/",
+        "teaser":null}]
